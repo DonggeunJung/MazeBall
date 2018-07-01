3dd1ab352d27075a4b57f044ecd648f92f96ddbb
DonggeunJung <topsan72@gmail.com>
2018-07-01 11:17:43 -0400

    Initial commit

---
+++ Assets/Scripts/Hole.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hole : MonoBehaviour {
+    public bool fallIn = false;
+    // 어떤 공을 빨아 들일지를 태그로 지정
+    public string activeTag;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == activeTag)
+        {
+            fallIn = true;
+        }
+    }
+
+    void OnGUI()
+    {
+        if( fallIn == true )
+            GUI.Label(new Rect(0, 0, 100, 30), "You Win. Very good. Game Clear!");
+    }
+}
